cca4d0776bfaca2ff8201978bafdf7be9a36c63b
hunghnuit <hunghn0999@gmail.com>
2020-11-24 00:00:28 +0700

    Add method of home to update more imgae in gallery if there is any request

--- src/Index/Home.js
+++ src/Index/Home.js
@@ -54,6 +54,36 @@ class Index extends Component {
         }
     }
 
+    async getMoreMedia(){
+        let newDataGallery = this.state.dataGallery;
+        // Demo an array of object, will be replaced by response later 
+        let tempArrObj = [{
+            "shortcode": "CBHY1ZaJEx5",
+            "url": "https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/101851983_2598122903839670_5522408425266996950_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&_nc_cat=1&_nc_ohc=nJynWahi_KUAX9X4-yt&tp=1&oh=06ae19a699da48ccc54c1974209a8ea3&oe=5FE5D413",
+            "isVideo": false,
+            "width": 1080,
+            "height": 1080,
+            "countComment": 24508,
+            "countLike": 1073592
+        },
+        {
+            "shortcode": "CBDB20spoxq",
+            "url": "https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/101717361_250895022906406_2163082289657108531_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&_nc_cat=1&_nc_ohc=J3m_zRxusX4AX875RkB&tp=1&oh=b880a830d89fad50dc62cf3860385709&oe=5FE48BB8",
+            "isVideo": false,
+            "width": 1080,
+            "height": 1080,
+            "countComment": 1665,
+            "countLike": 760570
+        }];
+        const updatedImagesData = newDataGallery.imagesData.concat(tempArrObj);
+        newDataGallery.imagesData = updatedImagesData;
+
+        // const updatedVideosData = newDataGallery.imagesData.concat(tempArrObj);
+        // newDataGallery.videosData = updatedVideosData;
+
+        this.setState({dataGallery: newDataGallery});
+    }
+
     onUpdateBannerInput (inputUrlElement, nameSocialNetwork) { 
         console.log("onUpdateBannerInput");
         this.setState({ 
@@ -68,14 +98,13 @@ class Index extends Component {
     }
 
     async componentDidUpdate(){
-        console.log("Home did update");
         if (this.state.clickedBtnSearch) {
-            console.log(`Searching because ${this.state.clickedBtnSearch}`);
-            // Note: if getMedia before setState will search twice due to async of js
+            console.log(`Searching because clickedBtnSearch is ${this.state.clickedBtnSearch}`);
+            // Note: if getMedia before setState will search at least twice due to async of js
             this.setState({clickedBtnSearch:false});
             await this.getMedia(this.state.inputUrlElement, this.state.nameSocialNetwork);
         } else {
-            console.log(`Not searching because ${this.state.clickedBtnSearch}`);
+            console.log(`Not searching because clickedBtnSearch is ${this.state.clickedBtnSearch}`);
         }
     }
 
@@ -90,7 +119,8 @@ class Index extends Component {
                     onUpdateBannerInput={this.onUpdateBannerInput.bind(this)}
                 ></Banner>
                 <Gallery 
-                    dataGallery= {this.state.dataGallery}>
+                    dataGallery= {this.state.dataGallery}
+                    getMoreMedia={this.getMoreMedia.bind(this)}>
                 </Gallery>
                 <Storage></Storage>
                 <AboutUs></AboutUs>
