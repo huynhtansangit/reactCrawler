fed1abfd55191184ae188aa1afbf13d53f04d5f4
hunghnuit <hunghn0999@gmail.com>
2020-12-30 08:34:43 +0700

    again

--- src/Index/Storage/Storage.js
+++ src/Index/Storage/Storage.js
@@ -30,40 +30,44 @@ class Storage extends React.Component {
         }
     }
 
-    async componentDidMount() {
-        // const accessToken = cookies.get("accessToken");
+    getCollectionList = () =>{
+        const accessToken = cookies.get("accessToken");
 
-        // let config = {
-        //     url: COLLECTIONS_URL,
-        //     method: 'GET',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'Authorization': `bearer ${accessToken}`
-        //     },
-        // };
+        let config = {
+            url: COLLECTIONS_URL,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `bearer ${accessToken}`
+            },
+        };
 
-        // axios.request(config)
-        //     .then(res => {
-        //         console.log(res.data);
-        //         return res.data
-        //     })
-        //     .then(data => {
-        //         if (data) {
-        //             this.setState({ isLoading: false, error: "" })
-        //             this.setState({ listCollectionId: data["collections"] })
-        //             // console.log(data["collections"])
-        //         }
-        //     })
-        //     .catch(error => {
-        //         console.log("Error occurred when trying to get your collection.");
-        //         if (error.response) {
-        //             this.setState({ isLoading: false, error: error.response.data['message'] })
-        //             alert(error.response.data.message);
-        //         }
-        //         else {
-        //             alert("Something went wrong. Please check your internet connection.");
-        //         }
-        //     })
+        axios.request(config)
+            .then(res => {
+                console.log(res.data);
+                return res.data
+            })
+            .then(data => {
+                if (data) {
+                    this.setState({ isLoading: false, error: "" })
+                    this.setState({ listCollectionId: data["collections"] })
+                    // console.log(data["collections"])
+                }
+            })
+            .catch(error => {
+                console.log("Error occurred when trying to get your collection.");
+                if (error.response) {
+                    this.setState({ isLoading: false, error: error.response.data['message'] })
+                    alert(error.response.data.message);
+                }
+                else {
+                    alert("Something went wrong. Please check your internet connection.");
+                }
+            })
+    }
+
+    componentDidMount(){
+        
     }
 
     render() {
@@ -71,17 +75,16 @@ class Storage extends React.Component {
         return (
             <section id="storage-section">
                 <div className="storage-container">
-                    <p>hello</p>
                     <img id="you-can-find-downloaded-stuff-here" src="Assets/Images/Store/Mask-Group-2-Copy.png" alt="All downloaded here" />
-                    {/* <Grid container spacing={0}> */}
-                    <Grid item xs={12} sm={12}>
-                        <Paper className={classes.paper}>
-                            <CollectionList
-                                isLoading={this.state.isLoading}
-                                listCollectionId={this.state.listCollectionId} />
-                        </Paper>
+                    <Grid container spacing={0}>
+                        <Grid item xs={12} sm={12}>
+                            <Paper className={classes.paper}>
+                                <CollectionList
+                                    isLoading={this.state.isLoading}
+                                    listCollectionId={this.state.listCollectionId} />
+                            </Paper>
+                        </Grid>
                     </Grid>
-                    {/* </Grid> */}
 
                 </div>
             </section>
